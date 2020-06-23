ac80dd84ca0d35d12c74b515f95aba2a9fa2ec3d
tobibako45 <tobibako45@gmail.com>
2020-06-23 11:56:58 +0900

    Add posts seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  Post.create!(
+      content: "今日も暇人 #{n+1}日目"
+  )
+end
